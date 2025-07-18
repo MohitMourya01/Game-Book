b88135bb0d8b26120484f7ab8fce59c1278885c1
Mohit_Mourya <key25mkm@gmail.com>
2025-07-18 13:36:19 +0530

    config fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,21 +1,42 @@
+// import { defineConfig, loadEnv } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig( (command, mode) => {
+//   const env = loadEnv(mode, process.cwd(), "")
+
+//   return {
+//   plugins: [react()],
+//   server: {
+//     ...command(env.VITE_DEBUG === "true" && {
+//     proxy: {
+//       "/api": {
+//         target: "http://localhost:8000",
+//         changeOrigin: true,
+//         secure: false
+//       }
+//     }
+//   }
+// ) }
+// }})
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig( (command, mode) => {
-  const env = loadEnv(mode, process.cwd(), "")
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
 
   return {
-  plugins: [react()],
-  server: {
-    ...command(env.VITE_DEBUG === "true" && {
-    proxy: {
-      "/api": {
-        target: "http://localhost:8000",
-        changeOrigin: true,
-        secure: false
-      }
+    plugins: [react()],
+    server: {
+      ...(env.VITE_DEBUG === "true" && {
+        proxy: {
+          "/api": {
+            target: "http://localhost:8000",
+            changeOrigin: true,
+            secure: false
+          }
+        }
+      })
     }
   }
-) }
-}})
+})
